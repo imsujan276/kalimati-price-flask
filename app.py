dfeab4d53be13cc933f085083e6b7f9801022d3a
Sujan Qode <sujan@emizr.net>
2022-06-13 18:13:43 +0545

    updated headers

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 URL = "https://kalimatimarket.gov.np/"
 SECRET = "lWqBcOoELieSOtVzoeJb_*Sv0$yx6QQzG7z@v)ieY=wlm(_!E3YQvFXL=6"
+HEADER = {
+  'User-Agent': 'Mozilla/6.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+  'Accept-Encoding': 'none',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive',
+  'refere': 'https://duckduckgo.com',
+  'cookie': """"""
+}
 
 @app.route("/")
 def home():
@@ -18,7 +28,8 @@ def home():
     if(dt != SECRET):
         return jsonify({"error": "A BIG NO NO"})
     try:
-        page = requests.get(URL, headers={'User-Agent': 'Mozilla/6.0'})
+        page = requests.get(URL, headers=HEADER)
+        # page = requests.get(URL, headers={'User-Agent': 'Mozilla/6.0'})
         soup = BeautifulSoup(page.content, "html.parser")
 
         priceTable = soup.find(id="commodityPricesDailyTable")
