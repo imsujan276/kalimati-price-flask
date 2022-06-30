05c74da3478ad1623e1d9a0515da03f7a9f8ff1b
Sujan Qode <sujan@emizr.net>
2022-06-30 18:05:57 +0545

    .

--- application/schedular.py
+++ application/schedular.py
@@ -2,6 +2,6 @@
 from . import utility
 
 def runSaveToDBSchedular(scheduler):
-    scheduler.add_job(func=utility.saveToDBCronJob, args=[], trigger='interval', id='saveToDB', seconds=5)
+    scheduler.add_job(func=utility.saveToDBCronJob, args=[], trigger='interval', id='saveToDB', seconds=60)
     scheduler.start()
     
